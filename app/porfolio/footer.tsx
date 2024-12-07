815396a380218e467cc6543834ac13a1aa28063a
Moe San <starlightastro@Moes-MacBook-Pro.local>
2024-12-07 18:29:14 -0500

    theme

--- app/porfolio/footer.tsx
+++ app/porfolio/footer.tsx
@@ -1,3 +1,5 @@
+import { ThemeSwitcher } from "../components/themeSwitcher";
+
  
 export function Footer() {
     return (
@@ -12,6 +14,7 @@ export function Footer() {
             <p className="text-gray-500">
                 © Polar Softwarer Inc. 2024
             </p>
+        <ThemeSwitcher/>
         
         </div>
 
